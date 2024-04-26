f4438157eea2bfd542f120f4c2e91024f03def3e
Atticfirm <Atticfirm@qq.com>
2024-04-26 10:35:35 +0800

    Second commit

--- KnapSack.cpp
+++ KnapSack.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
 const int n = 3;
-int KnapSack(int w[], int v[], int n, int C);
+int Knap(int w[], int v[], int n, int C);
 
 int main()
 {
 	int w[n] = { 10, 30, 20 }, v[n] = { 50, 120, 60 };
 	int C = 50;
-	int value = KnapSack(w, v, 3, C);
+	int value = Knap
+(w, v, 3, C);
 	cout << "背包获得的最大价值是：" << value << endl;
 	return 0;
 }
 
-int KnapSack(int w[], int v[], int n, int C)
+int Knap(int w[], int v[], int n, int C)
 {
-	double x[10] = { 0 };           //物品可部分装入
+	float x[10] = { 0 };           //物品可部分装入
 	int maxValue = 0, i;
 	for (i = 0; w[i] < C; i++)
 	{
@@ -22,7 +23,7 @@ int KnapSack(int w[], int v[], int n, int C)
 		maxValue += v[i];
 		C = C - w[i];             //背包剩余容量
 	}
-	x[i] = (double)C / w[i];        //物品i装入一部分
+	x[i] = (float)C / w[i];        //物品i装入一部分
 	maxValue += x[i] * v[i];
 	return maxValue;              //返回背包获得的价值
 }
